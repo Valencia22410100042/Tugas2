f254e005e98dedef6a45ef1722a26f81fbc7d98d
Valencia22410100042 <127917100+Valencia22410100042@users.noreply.github.com>
2024-03-26 11:33:23 +0700

    Add files via upload

---
+++ Tugas1.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+     <head>
+        <title>HTMLku</title>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+		<link rel="stylesheet" href="mycss.css">
+     </head>
+    <body>
+    <a href="Tugas11.php" target="_self"><i class="fas fa-plus button-icon" input type="button" name="+" value="+"></i></a>
+	<div style="overflow-x:auto;">
+    <table border="1">
+	<tr>
+	    <th rowspan="2">kode</th>
+        <th rowspan="2">nama barang</th>
+	    <th rowspan="2">satuan</th>
+	    <th colspan="2">harga</th>	   
+	    <th rowspan="2">Action</th>	
+	</tr>
+
+	<tr>             
+	    
+	     <th style="width:20%">beli</th>
+	     <th style="width:20%">jual</th>
+
+	<tr>
+	     <td>M01</td>
+         <td>cpu</td>
+	     <td>pcs</td>
+	     <td>200</td>
+	     <td>350</td>
+	     <td>
+           
+		    <a href="Tugas12.php" target="_self"><i class="fas fa-edit button-icon" input type="button" name="E" value="E"></i></a>
+           
+	     </td>		
+	</tr>
+
+    <tr>
+	     <td>M02</td>
+         <td>ram</td>
+	     <td>pcs</td>
+	     <td>300</td>
+	     <td>400</td>
+	     <td>
+         <a href="Tugas12.php" target="_self"><i class="fas fa-edit button-icon" input type="button" name="E" value="E"></i></a>
+	     </td>		
+	</tr>
+
+    <tr>
+	     <td>M03</td>
+         <td>vga</td>
+	     <td>pcs</td>
+	     <td>400</td>
+	     <td>500</td>
+	     <td>
+         <a href="Tugas12.php" target="_self"><i class="fas fa-edit button-icon" input type="button" name="E" value="E"></i></a>
+	     </td>		
+</tr>
+</table>
+</div>
+</body>
+</html>
---
+++ Tugas11.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<link rel="stylesheet" href="mycss.css">
+</head>
+<body>
+<p class="border">
+  <form actions="latihan2.php" method="POST">
+  <label for="kode">Kode:</label><br>
+  <input type="text" id="kode" name="kode"><br>
+  <label for="nama">Nama:</label><br>
+  <input type="text" id="nama" name="nama"><br>
+  <label for="satuan">Satuan:</label><br>
+  <input type="text" id="satuan" name="satuan"><br>
+  <label for="beli">Beli:</label><br>
+  <input type="text" id="beli" name="beli"><br> 
+  <label for="jual">Jual:</label><br>
+  <input type="text" id="jual" name="harga"><br> 
+
+  <a href="Tugas1.php" target="_self"><i class="fas fa-save buttoniconsave" input type="save" value="Save" name="save">  Save</i></a>
+  <a href="Tugas1.php" target="_self"><i class="fas fa-times buttoniconcancel" input type="cancel" value="Cancel" name="cancel">  Cancel</i></a>  
+</form>
+</p>
+</body>
+</html>
